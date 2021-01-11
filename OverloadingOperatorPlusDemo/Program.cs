624be2c5ee1d3297ab851532f8ea1b92334bf427
Volodymyr Siaskyi <volodymyr.siaskyi@gmail.com>
2021-01-11 18:38:17 +0200

    Add project files.

---
+++ OverloadingOperatorPlusDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingOperatorPlusDemo
+{
+    class MyClass
+    {
+        public int number;
+        public MyClass(int n)
+        {
+            number=n;
+        }
+        public static int operator+(MyClass a, MyClass b)
+        {
+            int m = a.number + b.number;
+            return m;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass A = new MyClass(100);
+            MyClass B = new MyClass(200);
+            int num;
+            num = A + B;
+            Console.WriteLine("A+B= "+num);
+        }
+    }
+}
